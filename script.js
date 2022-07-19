530ad99d2890e1347ed08b884bba7fc1c91a52a2
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-07-18 20:41:19 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let kaomoji = [['smile', '(´• ω •`)']]
 var color = null
 function setModeColor(){
     var pc = null
@@ -6,12 +7,20 @@ function setModeColor(){
     document.body.style.color = pc
     for (let img of document.getElementById('main').getElementsByTagName('img')){img.style.borderTop = 'solid ' + pc; img.style.borderBottom = 'solid' + pc}
     for (let a of document.body.getElementsByTagName('a')){a.style.color = pc}
+    for (let code of document.body.getElementsByClassName('code')){
+        if (code.tagName == 'DIV'){
+            if (pc == 'white'){code.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'}
+            else {code.style.backgroundColor = 'rgba(255, 255, 255, 0.4)'}
+        }
+    }
     document.body.style.backgroundColor = 'rgb(' + color['r'] + ',' + color['g'] + ',' + color['b'] + ')'
 }
 function setColor(){
-    ['r', 'g', 'b'].forEach(c => {color[c] = 255 - color[c]})
+    ['r', 'g', 'b'].forEach(c => {
+        if (color[c]<127){color[c]+=127}
+        else{color[c]-=127}
+    })
     setModeColor()
-    console.log(color)
 }
 function load(){
     var pageID = new URLSearchParams(window.location.search).get('p')
@@ -25,7 +34,14 @@ function load(){
             window.location.href = url.toString()
         }
         var page = data[data.findIndex(d => d.id == pageID)]
+        kaomoji.forEach(km => {
+            km[0] = '~~' + km[0] + '~~'
+            page['content'] = page['content'].split(km[0]).join(km[1])
+        })//.replace(/\n/g, '<br />');
         document.getElementById('main').innerHTML = '<h1 id="title">' + page['title'] + '</h1>' + page['content']
+        if (page['id']!='404'){document.getElementById('main').innerHTML += '<div id="date"><b>Created: </b>'+page['date']+'<br><b>Last Updated: </b>'+page['lastUpdated']+'</div>'}
+        Array.prototype.slice.call(document.getElementById('main').getElementsByTagName('img')).forEach(img => {img.src = 'https://raw.githubusercontent.com/Jeffser/Blog-Data/main/media/' + page['id'] + '/' + img.alt})
+        Array.prototype.slice.call(document.getElementById('main').getElementsByTagName('code')).forEach(code => {code.innerHTML = code.innerHTML.replace(/\n/g, '<br />')})
         color = page['color']
         setModeColor()
     })
