14694f649ef8e5517acd542e32f9754207a4c7b5
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-10-01 18:38:14 -0600

    Add files via upload

---
+++ tentri/script.js
@@ -0,0 +1,65 @@
+window.onload = function(){
+    $.getJSON('https://raw.githubusercontent.com/Jeffser/Blog-Data/main/activities.json', function(data){
+    var activities = []
+    var code = new URLSearchParams(window.location.search).get('code')
+    if (code!=null && code!=''){
+        code = code.split('-')
+        data.forEach(element => {
+            let tempDT = element[0].split(' ')
+            tempDT.length = code.length
+            if (tempDT.join(' ')==code.join(' ')){
+                let dateTime = element[0]
+                element.splice(0, 1)
+                activities.push({'dateTime': dateTime, 'activities': element})
+                ////----////
+                let html_div = document.createElement('div')
+                html_div.id = dateTime
+                html_div.className = 'activityList'
+                let html_dateTime = document.createElement('h1')
+                tempDT = dateTime.split(' ')
+                html_dateTime.innerHTML = tempDT[2] + '/' + tempDT[1] + '/' + tempDT[0] + ' ' + tempDT[3] + ':' + tempDT[4]
+                html_dateTime.className = 'dateTime'
+                html_div.appendChild(html_dateTime)
+                element.forEach(activity => {
+                    let html_subDiv = document.createElement('div')
+                    html_subDiv.className = 'activity'
+                    html_subDiv.style.backgroundImage = 'url(' + activity['large_image_url'] + ')'
+                    let html_name = document.createElement('h1')
+                    html_name.className = 'name'
+                    html_name.innerHTML = '<span>' + activity['name'] + '</span>'
+                    html_subDiv.appendChild(html_name)
+                    let html_details = document.createElement('h2')
+                    html_details.className = 'details'
+                    html_details.innerHTML = '<span>' + activity['details'] + '</span>'
+                    html_subDiv.appendChild(html_details)
+                    let html_state = document.createElement('h2')
+                    html_state.className = 'state'
+                    html_state.innerHTML = '<span>' + activity['state'] + '</span>'
+                    html_subDiv.appendChild(html_state)
+                    html_div.appendChild(html_subDiv)
+                })
+                document.getElementById('main').appendChild(html_div)
+                ////----////
+            }
+        });
+    }
+    })
+}
+function verf(element){
+    if (element.value==''){return true}
+    if (element.value>element.max){element.value = element.max}
+    else if (element.value<element.min){element.value = element.min}
+}
+function go(){
+    data = [document.getElementById('year').value, document.getElementById('month').value, document.getElementById('day').value, document.getElementById('hour').value, document.getElementById('minute').value]
+    page = ''
+    i = 0
+    while (data[i]!='' && i<data.length){
+        if (data[i].length==1){data[i]='0'+data[i]}
+        page+=data[i]+'-'
+        i+=1
+    }
+    page = page.slice(0,-1)
+    if (page==""){alert('You have to at least select the year')}
+    else{window.location.href='?code='+page}
+}
