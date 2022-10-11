58dd5fcf2573206dccca6d3dada4dbfc467bace4
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-10-11 13:23:45 -0600

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(window).on('load', function(){
-    map = ["tentri"];
+    map = ["tentri", "muse"];
     map.forEach(pageName => {
         $.getJSON("./"+pageName+"/metadata.json", function(page){
             $('#list-'+page['type']).append('<div onclick="window.location.href=\''+pageName+'\';" class="page" id="'+pageName+'"><h1 class="pageTitle">'+page['name']+'</h1><p class="pageDescription">'+page['description']+'</p></div>');
