416e3359ac466bcc5f3be1cd262a84fa87c8667b
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-10-11 12:54:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(window).on('load', function(){
     map = ["tentri"];
     map.forEach(pageName => {
-        $.getJSON(pageName+"/metadata.json", function(page){
+        $.getJSON("https://raw.githubusercontent.com/Jeffser/Jeffser.github.io/main/"+pageName+"/metadata.json", function(page){
             $('#list-'+page['type']).append('<div onclick="window.location.href=\''+pageName+'\';" class="page" id="'+pageName+'"><h1 class="pageTitle">'+page['name']+'</h1><p class="pageDescription">'+page['description']+'</p></div>');
         });
     });
