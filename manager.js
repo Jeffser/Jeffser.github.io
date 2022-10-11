8b84ec0f478409097cca70c96b541c85b34d0858
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-10-11 12:38:41 -0600

    Add files via upload

--- manager.js
+++ manager.js
@@ -11,6 +11,19 @@ function changeMode(){
     CSSVariables.forEach(variable => {$(':root').get(0).style.setProperty('--'+variable, modifyHex($(':root').css('--'+variable)));});
     if ($(window).scrollTop()>10) $(':root').get(0).style.setProperty('--headerBackgroundColor', 'rgba(' + $(':root').css('--backgroundColor').replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i,(m, r, g, b) => '#' + r + r + g + g + b + b).substring(1).match(/.{2}/g).map(x => parseInt(x, 16)).join() + ',0.9)');
     else $(':root').get(0).style.setProperty('--headerBackgroundColor', $(':root').css('--backgroundColor'));
+    $('head').get(0).append('<meta id="theme-color" name="theme-color" content="'+$(':root').css('--backgroundColor')+'">');
+}
+//RICH ALERT
+function richAlert(msg){
+    $('body').append('<div id="richAlertContainer"><div id="richAlert">'+msg+'</div></div>')
+    $('#richAlert').click(function(event){event.stopPropagation();});
+    $('#richAlertContainer').click(function(event){event.target.remove()})
+}
+//GET DEVELOPMENT STATUS
+function getDevelopmentStatus(){
+    $.getJSON('metadata.json', function(metadata){
+        richAlert('<h1 class="tertiaryText title">Development Status - '+metadata['name']+'</h1><div id="development"><div id="development-information"><h1 class="tertiaryText subtitle">'+metadata['development']['status'].charAt(0).toUpperCase()+metadata['development']['status'].slice(1)+'</h1>'+metadata['development']['note']+'</div><img src="../_globalMedia/development/'+metadata['development']['status']+'.gif" alt="'+metadata['development']['status']+'"></div>');
+    })
 }
 //METADATA
 $.getJSON('metadata.json', function(metadata){
@@ -20,18 +33,23 @@ $.getJSON('metadata.json', function(metadata){
         $(':root').get(0).style.setProperty('--'+key,v);
         CSSVariables.push(key);
     }
-    $('head').get(0).append('<meta id="theme-color" name="theme-color" content="'+metadata['CSSVariables']['backgroundColor']+'">');
+    $('head').get(0).append('<meta id="theme-color" name="theme-color" content="'+$(':root').css('--backgroundColor')+'">');
 })
 $(window).on('load', function() {
     //HEADER
-    $("header").html('<div><img src="https://avatars.githubusercontent.com/u/69224322?v=4" alt="logo" onclick="changeMode()"><b onclick="window.location.href=\'../\'">Jeffry\'s Corner</b></div>');
+    $("header").html('<div><img src="https://avatars.githubusercontent.com/u/69224322?v=4" alt="logo"><b onclick="window.location.href=\'../\'">Jeffry\'s Corner</b></div>');
+    $("header").find('img').contextmenu(function(e){e.preventDefault();})
+    $("header").find('img').mousedown(function(ev){
+        if(ev.which==1)changeMode();
+        if(ev.which==3){ev.preventDefault(); getDevelopmentStatus();}
+    });
     //FOOTER
     $("footer").html('Made by <a href="https://www.github.com/jeffser">JeffSER</a> with ❤️');
     //SCROLL THING
     window.onscroll = function(){
         let scroll = $(window).scrollTop();
         if (scroll<10){
-            $(':root').get(0).style.setProperty('--headerBoxShadow', 'none')
+            $(':root').get(0).style.setProperty('--headerBoxShadow', 'none');
             $(':root').get(0).style.setProperty('--headerBackgroundColor', $(':root').css('--backgroundColor'));
         }
         if (scroll<100){
---
+++ script.js
@@ -0,0 +1,9 @@
+$(window).on('load', function(){
+    map = ["tentri"];
+    map.forEach(pageName => {
+        $.getJSON(pageName+"/metadata.json", function(page){
+            $('#list-'+page['type']).append('<div onclick="window.location.href=\''+pageName+'\';" class="page" id="'+pageName+'"><h1 class="pageTitle">'+page['name']+'</h1><p class="pageDescription">'+page['description']+'</p></div>');
+        });
+    });
+
+})
