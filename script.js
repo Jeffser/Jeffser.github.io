8db21f35a4c6e4cf69157a845d98eebfe42e1342
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2022-10-12 17:48:51 -0600

    fixed

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 $(window).on('load', function(){
-    pages = [];
     types = ["project", "blog"];
     types.forEach(type =>{
         for (let i=0; i<$("#list-"+type).children(".page").length; i++){
-            pages.push($("#list-"+type).children(".page")[i].id);
+            let pageName = $("#list-"+type).children(".page")[i].id;
+            $.getJSON("./"+type+'/'+pageName+"/metadata.json", function(page){
+                $('#list-'+page['type']).find('#'+pageName).append('<h1 class="pageTitle">'+page['name']+'</h1><p class="pageDescription">'+page['description']+'</p>');
+                $('#list-'+page['type']).find('#'+pageName).click(function(){window.location.href=page['type']+'/'+pageName;});
+            });
         }
     })
-    pages.forEach(pageName => {
-        $.getJSON("./"+page['type']+'/'+pageName+"/metadata.json", function(page){
-            $('#list-'+page['type']).find('#'+pageName).append('<h1 class="pageTitle">'+page['name']+'</h1><p class="pageDescription">'+page['description']+'</p>');
-            $('#list-'+page['type']).find('#'+pageName).click(function(){window.location.href=page['type']+'/'+pageName;});
-        });
-    });
-
 })
