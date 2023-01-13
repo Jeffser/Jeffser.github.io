b13ac20802fc7b61c6820350f7c3858d450c4588
Jeffry Samuel Eduarte Rojas <69224322+Jeffser@users.noreply.github.com>
2023-01-12 21:02:28 -0600

    Add files via upload

--- kindling/script.js
+++ kindling/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 document.addEventListener('touchend', handleTouchEnd, false);
-var start, end, baseMargin, infoOpen
+var start, end, baseMargin, infoOpen, locked
+locked = false
 infoOpen = false
 function vwToPx(vw) {
     return document.documentElement.clientWidth * (vw / 100)
@@ -12,40 +13,57 @@ function isTouchingInformation(){
 }
 function handleTouchStart(evt) {
     start = { 'x': evt.changedTouches[0].clientX, 'y': evt.changedTouches[0].clientY, 'element': evt.srcElement }
-    console.log(start['element'].parentElement)
 };
 function handleTouchEnd(evt) {
     end = { 'x': evt.changedTouches[0].clientX, 'y': evt.changedTouches[0].clientY }
-    if (isTouchingInformation() && start['y'] - end['y'] > document.documentElement.clientHeight * .1) {
+    if (isTouchingInformation() && start['y'] - end['y'] > document.documentElement.clientHeight * .1 && infoOpen == false && document.getElementById('info').style.height == '30vh') {
         document.getElementById('info').style.height = 'calc(100% - 12vw - ' + document.getElementsByTagName('header')[0].offsetHeight + 'px)'
         document.getElementById('info').style.overflowY = 'auto'
         infoOpen = true
         return
     }
-    if (start['element'] == document.getElementById('nameAge') && start['y'] - end['y'] < document.documentElement.clientHeight * .1) {
+    if (start['element'] == document.getElementById('nameAge') && start['y'] - end['y'] > document.documentElement.clientHeight * .1 && document.getElementById('info').style.height == '15vh') {
         document.getElementById('info').style.height = '30vh'
         document.getElementById('info').style.overflowY = 'hidden'
         infoOpen = false
         return
     }
+    else if (start['element'] == document.getElementById('nameAge') && start['y'] - end['y'] < document.documentElement.clientHeight * .1 && (infoOpen == true || document.getElementById('info').style.height == '15vh')) {
+        document.getElementById('info').style.height = '30vh'
+        document.getElementById('info').style.overflowY = 'hidden'
+        infoOpen = false
+        return
+    }
+    else if (start['element'] == document.getElementById('nameAge') && start['y'] - end['y'] < document.documentElement.clientHeight * .1){
+        document.getElementById('info').style.height = '15vh'
+        document.getElementById('info').style.overflowY = 'hidden'
+        infoOpen = false
+        return
+    }
+    console.log(start['y'] - end['y'], document.documentElement.clientHeight * .1)
     //if (infoOpen) return
     document.getElementById('card').style.transition = '.2s'
+    locked = true
     if (document.getElementById('rightIndicator').style.opacity != '0') { document.getElementById('card').style.opacity = '0'; document.getElementById('card').style.marginLeft = document.documentElement.clientWidth + 'px'; document.getElementById('card').style.marginRight = '-' + document.documentElement.clientWidth + 'px'; document.getElementById('rightIndicator').style.opacity = '1'}
     if (document.getElementById('leftIndicator').style.opacity != '0') { document.getElementById('card').style.opacity = '0'; document.getElementById('card').style.marginRight = document.documentElement.clientWidth + 'px'; document.getElementById('card').style.marginLeft = '-' + document.documentElement.clientWidth + 'px'; document.getElementById('leftIndicator').style.opacity = '1'}
-    if (document.getElementById('rightIndicator').style.opacity == '0' && document.getElementById('leftIndicator').style.opacity == '0') document.getElementById('card').style.margin = baseMargin + 'px'
+    if (document.getElementById('rightIndicator').style.opacity == '0' && document.getElementById('leftIndicator').style.opacity == '0') {document.getElementById('card').style.margin = baseMargin + 'px'; locked=false; return}
     setTimeout(function () {
         document.getElementById('rightIndicator').style.opacity = '0'
         document.getElementById('leftIndicator').style.opacity = '0'
         document.getElementById('card').style.margin = baseMargin + 'px'
         document.getElementById('card').style.opacity = '1'
-        setTimeout(function () { document.getElementById('card').style.transition = '0s' }, 500)
+        setTimeout(function () {
+            document.getElementById('card').style.transition = '0s'
+            setTimeout(function(){locked = false}, 1500)
+        }, 1000)
     }, 500)
 
 }
 function handleTouchMove(evt) {
+    if (locked || infoOpen) return
     const prc = (evt.changedTouches[0].clientX - (document.documentElement.clientWidth / 2)) * 2 / document.documentElement.clientWidth
     if (Math.abs(start['y'] - evt.changedTouches[0].clientY)*1.5 > Math.abs(start['x'] - evt.changedTouches[0].clientX)) return
-    if (infoOpen) return //isTouchingInformation()||
+    //isTouchingInformation()||
     if (evt.changedTouches[0].clientX - start['x'] > 0) {
         document.getElementById('card').style.marginRight = baseMargin / (1 + prc) + 'px'
         document.getElementById('card').style.marginLeft = (1 + prc) * baseMargin + 'px'
@@ -62,7 +80,10 @@ function handleTouchMove(evt) {
     else document.getElementById('rightIndicator').style.opacity = '0'
 };
 window.onload = function () {
+    document.getElementById('leftIndicator').style.opacity = '0'
+    document.getElementById('rightIndicator').style.opacity = '0'
     baseMargin = vwToPx(4)
     document.getElementById('card').style.margin = baseMargin + 'px'
-
+    document.getElementById('info').style.height = '30vh'
+    document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#5d5dd0');
 }
