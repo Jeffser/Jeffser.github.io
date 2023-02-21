f85f4a87befdc6ef1e5a26b65a898fd2ec385966
Jeffser <jeffrywalfa@gmail.com>
2023-02-20 20:00:28 -0600

    update script.js and style.css

--- code/muse/script.js
+++ code/muse/script.js
@@ -1,9 +1,12 @@
+id = null;
 $(window).on('load', function(){
     $.getJSON('data.json', function(data){
         song = data[Math.floor(Math.random()*data.length)];
         $(':root').get(0).style.setProperty('--color-hue', song['hue']);
         $('#title').html('<a style="text-decoration: none;" href="https://music.youtube.com/watch?v=' + song['id'] + '">' + song['title'] + '</a>');
         $('#author').html('<a style="text-decoration: none;" href="https://music.youtube.com/channel/' + song['author_id'] + '">' + song['author'] + '</a>');
-        $('#youtube-container').html('<iframe autoplay width="100%" height="100%" src="https://www.youtube.com/embed/' + song['id'] + '?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+        $('#youtube-container').css('background-image', 'url(https://i3.ytimg.com/vi/' + song['id'] + '/maxresdefault.jpg)')
+        id = song['id'];
+        $("#youtube-container").mousedown(function(){$('#youtube-container').html('<iframe autoplay width="100%" height="100%" src="https://www.youtube.com/embed/' + id + '?controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');});
     });
 });
