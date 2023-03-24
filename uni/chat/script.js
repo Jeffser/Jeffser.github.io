f8932abb2a310634ae0749ffb308a2ed5e23ef89
JeffSER <jeffrywalfa@gmail.com>
2023-03-23 23:20:20 -0600

    testing

--- uni/chat/script.js
+++ uni/chat/script.js
@@ -1,4 +1,12 @@
 function showMessage(sender, msg){
     $("#chat").append('<div class="message-box"><div class="' + sender + '">' + msg + '</div></div>');
     $("#chat").scrollTop($("#chat")[0].scrollHeight);
+    $.ajax({
+        dataType: "json",
+        method: "POST",
+        url: "https://jeffser.com/chat.php",
+        data: {pass: "samuel1202", text: msg},
+    }).done(function(data){
+        console.log(data);
+    });
 }
