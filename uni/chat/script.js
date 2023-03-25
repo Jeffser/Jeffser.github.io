e8a3c84df59f981f7328cd74bdfeda7a9f3ca0a8
JeffSER <jeffrywalfa@gmail.com>
2023-03-25 12:27:32 -0600

    updated

--- uni/chat/script.js
+++ uni/chat/script.js
@@ -1,11 +1,18 @@
+key = ''
+id = ''
+$(window).on('load', function(){
+    key = prompt('Llave');
+    id = new Date().toLocaleString();
+});
+
 function showMessage(sender, msg){
     $("#chat").append('<div class="message-box"><div class="' + sender + '">' + msg + '</div></div>');
     $("#chat").scrollTop($("#chat")[0].scrollHeight);
     $.ajax({
         dataType: "json",
         method: "POST",
-        url: "https://jeffser.com/chat.php",
-        data: {pass: "samuel1202", text: msg, id: "asdasda"},
+        url: "http://api.jeffser.com/api/chat/message",
+        data: {key: key, msg: msg, id: id},
     }).done(function(data){
         console.log(data);
     });
