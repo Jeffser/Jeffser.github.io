804501e0180da5529aad0996405ad4b76b47b9f4
JeffSER <jeffrywalfa@gmail.com>
2023-03-25 12:29:38 -0600

    updated

--- uni/chat/script.js
+++ uni/chat/script.js
@@ -5,15 +5,24 @@ $(window).on('load', function(){
     id = new Date().toLocaleString();
 });
 
-function showMessage(sender, msg){
-    $("#chat").append('<div class="message-box"><div class="' + sender + '">' + msg + '</div></div>');
-    $("#chat").scrollTop($("#chat")[0].scrollHeight);
+function getReply(msg){
     $.ajax({
         dataType: "json",
         method: "POST",
         url: "http://api.jeffser.com/api/chat/message",
         data: {key: key, msg: msg, id: id},
     }).done(function(data){
-        console.log(data);
+        if (data['status'] == 'success'){
+            showMessage('bot', data['message'])
+        }
+        else{
+            console.log(data)
+        }
     });
+}
+
+function showMessage(sender, msg){
+    $("#chat").append('<div class="message-box"><div class="' + sender + '">' + msg + '</div></div>');
+    $("#chat").scrollTop($("#chat")[0].scrollHeight);
+    if (sender == 'user') getReply(msg);
 }
