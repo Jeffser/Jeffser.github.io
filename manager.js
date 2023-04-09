856fb0c55d876cf7bb22cfa0e58ac4a988f33448
JeffSER <jeffrywalfa@gmail.com>
2023-04-08 23:14:20 -0600

    fixed header scale when refresh

--- manager.js
+++ manager.js
@@ -12,29 +12,34 @@ function richAlert(msg, width='70vw', block=false){
 function showDevelopmentStatus(status, note){
     richAlert('<section id="development"><section id="development-information" style="text-align: center"><h1>Desarrollo</h1><h2>'+status.charAt(0).toUpperCase()+status.slice(1)+'</h2>'+note+'</section><img src="https://jeffser.github.io/globalMedia/development/'+status+'.gif" alt="'+status+'"></section>');
 }
+function checkHeaderPosition(){
+    if (activeEG) return;
+    let scroll = $(window).scrollTop();
+    if (scroll<10){
+        $(':root').get(0).style.setProperty('--header-scale', '12vmin');
+        $(':root').get(0).style.setProperty('--header-border', 'solid transparent 0px');
+        $(':root').get(0).style.setProperty('--header-background-opacity', '0');
+    }
+    else if (scroll<100){
+        $(':root').get(0).style.setProperty('--header-scale', 12-(scroll/100*5)+'vmin');
+        $(':root').get(0).style.setProperty('--header-background-opacity', '.7');
+        $(':root').get(0).style.setProperty('--header-border', 'solid 5px');
+    }
+    else {
+        $(':root').get(0).style.setProperty('--header-scale', '8vmin');
+        $(':root').get(0).style.setProperty('--header-background-opacity', '.7');
+        $(':root').get(0).style.setProperty('--header-border', 'solid 5px');
+    }
+}
+
 $(window).on('load', function(){
     if ($("#custom-favicon").length == false) $("head").append('<link rel="icon" type="image/x-icon" href="https://avatars.githubusercontent.com/u/69224322?v=4">');
     $("header:not(.custom)").html('<div><img src="https://avatars.githubusercontent.com/u/69224322?v=4" alt="logo"><b onclick="window.location.href=\'https://jeffser.github.io\'">Jeffry\'s Corner</b></div>');
     $("header").find('img').contextmenu(function(e){e.preventDefault(); showDevelopmentStatus(getComputedStyle(document.documentElement).getPropertyValue('--desktop-development-status'), getComputedStyle(document.documentElement).getPropertyValue('--desktop-development-note'));})
     $("header").find('img').mousedown(function(ev){if(ev.which==1) changeMode();});
     $("footer:not(.custom)").html(getComputedStyle(document.documentElement).getPropertyValue('--publish-date') + '<br>Made by <a href="https://www.github.com/jeffser">JeffSER</a> with ❤️');
-    window.onscroll = function(){
-        if (activeEG) return;
-        let scroll = $(window).scrollTop();
-        if (scroll<10){
-            $(':root').get(0).style.setProperty('--header-scale', '12vmin');
-            $(':root').get(0).style.setProperty('--header-border', 'solid transparent 0px');
-            $(':root').get(0).style.setProperty('--header-background-opacity', '0');
-        }
-        else if (scroll<100){
-            $(':root').get(0).style.setProperty('--header-scale', 12-(scroll/100*5)+'vmin');
-            $(':root').get(0).style.setProperty('--header-background-opacity', '.7');
-            $(':root').get(0).style.setProperty('--header-border', 'solid 5px');
-        }
-        else {
-            $(':root').get(0).style.setProperty('--header-scale', '8vmin');
-        }
-    }
+    window.onscroll = checkHeaderPosition;
+    checkHeaderPosition();
     var theme="light";
     if(localStorage.getItem("theme")) theme = localStorage.getItem("theme");
     else if(!window.matchMedia) return false;
