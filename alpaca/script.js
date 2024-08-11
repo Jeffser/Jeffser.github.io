7e7d090d8c09a29448fc331d8eaa8c7092692d30
Your Name <you@example.com>
2024-08-12 00:49:20 +0200

    new website (again)

--- alpaca/script.js
+++
@@ -1,8 +0,0 @@
-document.oncontextmenu = new Function("return false;")
-window.ondragstart = function () { return false }
-
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '#image-carousel',{
-        type:'loop',
-    } ).mount();
-  } );  
